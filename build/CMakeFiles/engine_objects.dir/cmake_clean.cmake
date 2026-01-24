9f4f9db032f1928451f4cbfe76efd6b9d8bb87a1
jmfp <therealjmfp@gmail.com>
2026-01-24 08:10:13 -0500

    should be the last time commiting junk

---
+++ build/CMakeFiles/engine_objects.dir/cmake_clean.cmake
@@ -0,0 +1,37 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/engine_objects.dir/include/glad/glad.c.o"
+  "CMakeFiles/engine_objects.dir/include/glad/glad.c.o.d"
+  "CMakeFiles/engine_objects.dir/src/Camera.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/Camera.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/Component.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/Component.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/GameObject.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/GameObject.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/Light.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/Light.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/Mesh.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/Mesh.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/Model.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/Model.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/Procedural.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/Procedural.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/Renderer.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/Renderer.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/Scene.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/Scene.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/Shader.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/Shader.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/Texture.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/Texture.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/Transform.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/Transform.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/Window.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/Window.cpp.o.d"
+  "CMakeFiles/engine_objects.dir/src/stb_image.cpp.o"
+  "CMakeFiles/engine_objects.dir/src/stb_image.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/engine_objects.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/engine_objects.dir/src/Component.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/engine_objects.dir/src/Component.cpp.o: \
+  /Users/jesseprice/Development/Personal/Britemune/OpenGL/src/Component.cpp
